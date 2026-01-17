7551f07e357d485e7a671928cf46109a885777b8
Kyle Merrill <kylemerrilltrade@gmail.com>
2026-01-16 17:05:49 -0700

    Definitive fix for Attendance charts SSR error

--- components/AttendanceCharts.js
+++ components/AttendanceCharts.js
@@ -1,10 +1,12 @@
-import NoSSR from './NoSSR';
-import AttendanceChartsClient from './AttendanceChartsClient';
+'use client'
+
+import dynamic from 'next/dynamic';
+
+const AttendanceChartsClient = dynamic(() => import('./AttendanceChartsClient'), {
+    ssr: false,
+    loading: () => <div style={{ height: 300, background: 'rgba(255,255,255,0.02)', borderRadius: '8px', marginTop: '1rem' }} />
+});
 
 export default function AttendanceCharts(props) {
-    return (
-        <NoSSR fallback={<div style={{ height: 300, background: 'rgba(255,255,255,0.02)', borderRadius: '8px', marginTop: '1rem' }} />}>
-            <AttendanceChartsClient {...props} />
-        </NoSSR>
-    );
+    return <AttendanceChartsClient {...props} />;
 }
